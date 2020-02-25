5965dd2caf2330f13205edb1fdef622a3e1094ae
Rexhep Ahmeti <Rexhepahmeti6@gmail.com>
2020-02-25 12:15:46 +0100

    initial commit

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router  = express.Router();
 const adressa = require('../models/crmModel');
     
-
+router.use(express.json());
 
 router.get('/', (req, res) =>{
     res.send(adressa);
 });
 
-router.get('/:id', (req, res) =>{
+router.get('/adress/:id', (req, res) =>{
     
     const adress = adressa.find(c => c.id === parseInt(req.params.id)); 
     if(!adress) 
@@ -17,6 +17,32 @@ router.get('/:id', (req, res) =>{
     }
     res.send(adress);
 });  
+
+router.post('/adress' , (req, res) =>{
+    const adres ={
+        id: adressa.length + 1,
+        name: req.body.name,
+        city: req.body.name,
+        country: req.body.name
+    }
+    adressa.push(adres);
+    res.send(adres);
+});
+
+router.put('/adress/:id', (req, res) =>{
+    const adress = adressa.find(c => c.id === parseInt(req.params.id)); 
+    if(!adress) 
+    {
+        res.status(404).send('Nuk eshte gjetur adressa me ID e dhene');
+    }
+    
+
+    adress.name = req.body.name;
+    adress.city = req.body.city;
+    adress.country = req.body.country;
+
+    res.send(adress);
+});
 
 
 module.exports = router;
